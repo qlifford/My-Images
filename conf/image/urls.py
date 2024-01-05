63ce871b810bb3f04afad1b6e3eb9fdbdd80ea2f
qlifford <otienocee@gmail.comgit config --global user.nameqlofford>
2024-01-05 15:13:24 +0300

    commit

--- conf/image/urls.py
+++ conf/image/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('', index, name = 'index'),
+    path('delete/<str:pk>', delete_image, name = 'delete'),
+    path('details/<str:pk>', detail, name = 'details'),
 ]
--- conf/image/views.py
+++ conf/image/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Image
 from .forms import ImageForm
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -10,5 +11,16 @@ def index(request):
     if request.method == 'POST':
         form = ImageForm(request.POST, request.FILES)
         form.save()
+        messages.success(request, 'Image Uploaded successfully !')
         return redirect('/')
     return render(request, 'image/index.html', context)
+
+def delete_image(request, pk):
+    image = Image.objects.get(pk=pk)
+    image.delete()
+    messages.success(request, 'Image deleted!')
+    return redirect('/')
+
+def detail(request, pk):
+    image = Image.objects.get(pk=pk)
+    return render(request, 'image/detail.html', {'image': image})
